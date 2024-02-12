5a21ee5f1e67d3e01b8e73bd068a75b3f2ab32f3
penzboti <penzboti@gmail.com>
2024-02-12 20:27:40 +0100

    site is functional, slight restructuring

--- code-translator/scripts/binary.js
+++ code-translator/scripts/binary.js
@@ -15,22 +15,9 @@ document.addEventListener("keydown", e => {
             break;
     }
 });
-
-currCode = "";
-function newCode() {
-    characters = "";
-    let n = Math.floor(Math.random() * 256);
-    currCode = n.toString(2).padEnd(8, "0");
-    displayBinary();
-}
-
-function displayBinary() {
-    for (let i = 0; i <= 7; i++) {
-        codeSequence.children[i].innerText = typeof(characters[i]) != "undefined" ? characters[i] : " ";
-    }
-}
-
+    
 let characters = "";
+// adds a character
 function addChar(char) {
     // if (currword.length <= userword.length) {return;}
     if (characters.length != 8) characters = `${characters}${char}`;
@@ -38,14 +25,47 @@ function addChar(char) {
     displayBinary();
 }
 
+// deletes a character
 function delChar() {
     characters = characters.slice(0, -1);
 
     displayBinary();
 }
 
+let answerPhase = false;
+// confirms the number and displays answer to the user
 function confirmNum() {
-    console.log(parseInt(characters, 2));
+    if (!answerPhase) {
+        let n = parseInt(characters, 2);
+        if (n == currN) {
+            baseSequence.classList.add("green");
+        } else {
+            baseSequence.innerHTML = `${currN}≠<span class="red">${n}</span>`;
+        }
+    }
+    else {
+        baseSequence.classList.remove("green");
+        newCode();
+    }
+    answerPhase = !answerPhase;
 }
 
+currN = "";
+// generates a new binary code
+function newCode() {
+    characters = "";
+    currN = Math.floor(Math.random() * 256);
+    // let currCode = n.toString(2).padEnd(8, "0");
+    baseSequence.innerText = currN;
+    displayBinary();
+}
+
+// displays the binary code
+function displayBinary() {
+    for (let i = 0; i <= 7; i++) {
+        codeSequence.children[i].innerText = typeof(characters[i]) != "undefined" ? characters[i] : " ";
+    }
+}
+
+// start the site
 newCode();
