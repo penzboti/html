e11acbc7da14d68c9a600e192c31bf5f5b1082ac
penzboti <penzboti@gmail.com>
2024-02-18 18:00:39 +0100

    readme & keyboard output area

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
-https://penzboti.github.io/html/
+website: https://penzboti.github.io/html/
 # ideas
- - Deutsche-multido to do regular german word practice aswell
  - copy Qlocktwo's clock desing
- - copy keybr.com's keyboard desing
 
 # tasks
 ## deutsche-multido
  - [ ] add complete list of words, in a popup
  - hun & eng version of the site?
-``` it's kind of mixed at the moment, so idk ```
+```it's kind of mixed at the moment, so idk ```
  - score system?
-``` an correct/incorrect answer percentage at the bottom should be fine. altough when starting the quiz again there should be an indicator ```
+```a correct/incorrect answer percentage at the bottom should be fine. altough when starting the quiz again there should be an indicator ```
+## keyboard remake
+ - [ ] full keyboard support, special characters 
+```the ability to type them aswell as able to view them on the keyboard when the special key is pressed (like shift or ctrl)```
+```this probably means more javascript instead of html, probably will have to decide between e.key || e.code```
+ - [ ] some apple macbook bug, probably with keycode
+ - [ ] the ability to touch buttons so i can show it off on mobile aswell
+ - [ ] some small extras, activatable by codes
--- remakes/keyboard/script.js
+++ remakes/keyboard/script.js
@@ -5,41 +5,51 @@
 // keyboard event util
 // https://www.toptal.com/developers/keycode
 
+// sets up id's for all keys, so they can be styled and handled
+// it only sets an id if the element does not have one.
+// the id is being set to the text of the key
 function setupKeys() {
     Array.from(document.getElementById('content').children).forEach(e => {
         Array.from(e.children).forEach(f => {
             if (f.id == "") {
                 let key = f.textContent;
                 f.id = key;
-                // console.log(f.id);
             }
         });
     });
 }
 
+// checking keydowns and keyups
 document.addEventListener('keydown', e => {
     if (!e.repeat) {
         if (e.code == "Tab" || e.code == "Digit1" || e.code == "Digit6") e.preventDefault();
-        // console.log(e.code + ' down');
+        console.log(e.code + ' down');
         toggleKey(e, true);
     }
 });
-
 document.addEventListener('keyup', e => {
-    // console.log(e.code + ' up');
+    console.log(e.code + ' up');
     toggleKey(e, false);
 });
 
+// handling styling keypresses, and back to normal
+// it does separate .keys and .codes, because the id's have been setup that way
 function toggleKey(e, down) {
     let checkCode = document.getElementById(e.code);
     if (checkCode !== null) {
-            if (down) document.getElementById(e.code).classList.add("pushed");
+            if (down) {
+                document.getElementById(e.code).classList.add("pushed");
+                handleText(e, "code");
+            }
             else document.getElementById(e.code).classList.remove("pushed");
     }
     else {
         let checkKey = document.getElementById(e.key.toLowerCase());
         if (checkKey !== null) {
-            if (down) document.getElementById(e.key.toLowerCase()).classList.add("pushed");
+            if (down) {
+                document.getElementById(e.key.toLowerCase()).classList.add("pushed");
+                handleText(e.key, "key");
+            }
             else document.getElementById(e.key.toLowerCase()).classList.remove("pushed");
         }
         else {
@@ -49,6 +59,7 @@ function toggleKey(e, down) {
 }
 
 // alt tabbing does not leave alt key in pushed style
+// basically checking for leaving the document
 document.addEventListener('blur', e => {
     Array.from(document.getElementById('content').children).forEach(e => {
         Array.from(e.children).forEach(f => {
@@ -57,4 +68,31 @@ document.addEventListener('blur', e => {
     });
 });
 
+let text = "";
+// output handling, .keys and .codes
+function handleText(event, mode) {
+    if (mode == "code") {
+        if (event.code == "Space") handleText(" ", "key");
+        if (event.code.includes("Digit")) {
+            handleText(event.code[5], "key");
+        }
+        if (event.code == "Backspace") {
+            text = text.slice(0, -1);
+        }
+        if (event.code == "Backquote") handleText("0", "key");
+        if (event.code == "Enter") {
+            // checkCode(text); // secret codes will be checked later here
+            text = "";
+        }
+        if (["Comma", "Period", "Slash"].includes(event.code)) {
+            handleText(event.key, "key");
+        }
+    }
+    if (mode == "key") {
+        text = `${text}${event}`;
+    }
+    document.getElementById('output').innerHTML = text;
+}
+
+// start the site
 setupKeys();
