fedcb3c9bb5e397fe2a1c6148a97899b0ce4399d
penzboti <penzboti@gmail.com>
2024-02-19 18:45:28 +0100

    code cleaning with setupkeys rewrite. special characters are coming.

--- README.md
+++ README.md
@@ -10,9 +10,9 @@ website: https://penzboti.github.io/html/
  - score system?
 ```a correct/incorrect answer percentage at the bottom should be fine. altough when starting the quiz again there should be an indicator ```
 ## keyboard remake
- - [ ] full keyboard support, special characters 
+ - [ ] full keyboard support, special characters ```in the works```
 ```the ability to type them aswell as able to view them on the keyboard when the special key is pressed (like shift or ctrl)```
 ```this probably means more javascript instead of html, probably will have to decide between e.key || e.code```
- - [ ] some apple macbook bug, probably with keycode
+ - [ ] some apple macbook bug, probably NOT with keycode
  - [ ] the ability to touch buttons so i can show it off on mobile aswell
  - [ ] some small extras, activatable by codes
--- remakes/keyboard/script.js
+++ remakes/keyboard/script.js
@@ -5,17 +5,51 @@
 // keyboard event util
 // https://www.toptal.com/developers/keycode
 
+// bunch of id presets, needed for setupkeys()
+const numberrowids = ["Backquote", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0", "Minus", "Equal"]
+const toprowids = ["BracketLeft", "BracketRight", "Backslash"];
+const midrowendids = ["Semicolon", "Quote"];
+const botrowendids = ["Comma", "Period", "Slash"];
+const rows = [numberrowids, toprowids, midrowendids, botrowendids];
+
+// next update sneak peek
+const keys = "0123456789öüóqwertzuiopőúűasdfghjkléáíyxcvbnm,.-";
+const shiftkeys = "§'\"+!%/=()ÖÜÓQWERTZUIOPŐÚŰASDFGHJKLÉÁÍYXCVBNM:_?";
+const ctrlkeys = " ~ˇ^˘°˛`˙´˝¨¸\\|Ä®™ €Í÷×¤äđĐ[]   Ł$ß<>#&@{} ;>*"
+
 // sets up id's for all keys, so they can be styled and handled
-// it only sets an id if the element does not have one.
-// the id is being set to the text of the key
+// it only sets an id if the element does not have one. this basically means non-function keys, only keys that return text.
+// the id is being set to the text of the key, or if it has a special code, then it sets it to that.
 function setupKeys() {
+    let row = 0;
     Array.from(document.getElementById('content').children).forEach(e => {
-        Array.from(e.children).forEach(f => {
-            if (f.id == "") {
-                let key = f.textContent;
-                f.id = key;
+        if (!e.classList.contains("sysrow")) {
+            currow = rows[row];
+            // seems like a great resource for the rust / linq way
+            // https://gist.github.com/DanDiplo/30528387da41332ff22b
+            let list = Array.from(e.children).filter(f => f.id == "");
+            let idlist = [];
+            for(i = 0; i < (list.length-currow.length); i++){
+                if (list[i].innerText == "z") {
+                    idlist.push(`KeyY`);
+                    continue;
+                }
+                if (list[i].innerText == "y") {
+                    idlist.push(`KeyZ`);
+                    continue;
+                }
+                idlist.push(`Key${list[i].innerText.toUpperCase()}`);
             }
-        });
+            if (row == 3) idlist.splice(0, 1, "IntlBackslash");
+            for(i = 0; i < currow.length; i++){
+                idlist.push(currow[i]);
+            }
+            list.forEach( e => {
+                e.id = idlist.shift();
+            });
+        }
+
+        row++;
     });
 }
 
@@ -28,7 +62,7 @@ document.addEventListener('keydown', e => {
     }
 });
 document.addEventListener('keyup', e => {
-    console.log(e.code + ' up');
+    // console.log(e.code + ' up');
     toggleKey(e, false);
 });
 
@@ -42,19 +76,6 @@ function toggleKey(e, down) {
                 handleText(e, "code");
             }
             else document.getElementById(e.code).classList.remove("pushed");
-    }
-    else {
-        let checkKey = document.getElementById(e.key.toLowerCase());
-        if (checkKey !== null) {
-            if (down) {
-                document.getElementById(e.key.toLowerCase()).classList.add("pushed");
-                handleText(e.key, "key");
-            }
-            else document.getElementById(e.key.toLowerCase()).classList.remove("pushed");
-        }
-        else {
-            // console.log(e.code + " not found");
-        }
     }
 }
 
@@ -73,22 +94,24 @@ let text = "";
 function handleText(event, mode) {
     if (mode == "code") {
         if (event.code == "Space") handleText(" ", "key");
-        if (event.code.includes("Digit")) {
-            handleText(event.code[5], "key");
-        }
-        if (event.code == "Backspace") {
+        else if (event.code == "Backspace") {
             text = text.slice(0, -1);
         }
-        if (event.code == "Backquote") handleText("0", "key");
-        if (event.code == "Enter") {
+        else if (event.code == "Backquote") handleText("0", "key");
+        else if (event.code == "Enter") {
             // checkCode(text); // secret codes will be checked later here
             text = "";
         }
-        if (["Comma", "Period", "Slash"].includes(event.code)) {
+        else if (["Comma", "Period", "Slash"].includes(event.code)) {
+            handleText(event.key, "key");
+        }
+        // skipping unused codes
+        else if (["ControlLeft", "Tab", "ShiftLeft", "AltLeft", "ShiftRight", "CapsLock", "AltRight", "ControlRight", "MetaLeft"].includes(event.code)) {}
+        else {
             handleText(event.key, "key");
         }
     }
-    if (mode == "key") {
+    if (mode == "key" && event != "Dead") {
         text = `${text}${event}`;
     }
     document.getElementById('output').innerHTML = text;
