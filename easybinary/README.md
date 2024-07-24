7486bf293963f6ce35faec730fdd6f3112df2a1a
penzboti <penzboti@gmail.com>
2024-07-24 21:06:11 +0200

    added favicon, routed old project easybinary, some navigation updates

---
+++ easybinary/README.md
@@ -0,0 +1,2 @@
+do not look at the code of this project.
+i found tailwind cool, so i've decided to use it.
--- source.js
+++ source.js
@@ -1,11 +1,18 @@
+// just plug this file anywhere you want a source button to appear
+
 let body = document.getElementsByTagName("body")[0];
 let node = document.createElement("p");
+
 node.innerHTML = "Source on <a href='https://github.com/penzboti/html'>GitHub</a>";
+node.id = "source";
+
 node.style.position = "fixed";
-node.style.top = "100svh"
+node.style.top = "100dvh"
+node.style.left = "0px";
 node.style.transform = "translate(5%, -200%)";
+node.style.backgroundColor = "rgba(255, 255, 255, 0.3)";
 // https://stackoverflow.com/questions/6338217/get-a-css-value-with-javascript
 node.style.color = window.getComputedStyle(body).getPropertyValue("background-color") == "rgba(0, 0, 0, 0)" ? "black" : "white";
 node.style.left = "0";
-node.id = "source";
+
 body.appendChild(node);
