34cecfa6f3b48aa4055e44e11148120cc2a2a1ab
penzboti <penzboti@gmail.com>
2025-03-24 17:42:15 +0100

    CODES formatting; MORSE word length & ux fix

--- code-translator/scripts/binary.js
+++ code-translator/scripts/binary.js
@@ -1,86 +1,90 @@
 // keybord input
-document.addEventListener("keydown", e => {
-    switch(e.key) {
-        case " ":
-            confirmNum();
-            break;
-        case "0":
-            addChar("0");
-            break;
-        case "1":
-            addChar("1");
-            break;
-        case "Backspace":
-            delChar();
-            break;
-    }
+document.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case " ":
+      confirmNum();
+      break;
+    case "0":
+      addChar("0");
+      break;
+    case "1":
+      addChar("1");
+      break;
+    case "Backspace":
+      delChar();
+      break;
+  }
 });
-    
+
 let characters = "";
 // adds a character
 function addChar(char) {
-    // if (currword.length <= userword.length) {return;}
-    if (characters.length != 8) characters = `${characters}${char}`;
+  if (answerPhase) return;
+  // if (currword.length <= userword.length) {return;}
+  if (characters.length != 8) characters = `${characters}${char}`;
 
-    displayBinary();
+  displayBinary();
 }
 
 // deletes a character
 function delChar() {
-    characters = characters.slice(0, -1);
+  if (answerPhase) return;
+  characters = characters.slice(0, -1);
 
-    displayBinary();
+  displayBinary();
 }
 
 let answerPhase = false;
 // confirms the number and displays answer to the user
 function confirmNum() {
-    if (!answerPhase) {
-        characters = characters.padEnd(8, "0");
-        let n = parseInt(characters, 2);
-        if (n == currN) {
-            baseSequence.classList.add("green");
+  if (!answerPhase) {
+    characters = characters.padEnd(8, "0");
+    let n = parseInt(characters, 2);
+    if (n == currN) {
+      baseSequence.classList.add("green");
+    } else {
+      baseSequence.innerHTML = `${currN}≠<span class="red">${n}</span>`;
+      let code = currN.toString(2).padStart(8, "0");
+      console.log(characters, code);
+      console.log(currN, n);
+      for (let i = 0; i <= 7; i++) {
+        if (code[i] != characters[i]) {
+          codeSequence.children[i].innerHTML =
+            `<span class="red">${typeof characters[i] != "undefined" ? characters[i] : "0"}</span>`;
         } else {
-            baseSequence.innerHTML = `${currN}≠<span class="red">${n}</span>`;
-            let code = currN.toString(2).padStart(8, "0");
-            console.log(characters, code)
-            console.log(currN, n)
-            for (let i = 0; i <= 7; i++) {
-                if (code[i] != characters[i]) {
-                    codeSequence.children[i].innerHTML = `<span class="red">${typeof(characters[i]) != "undefined" ? characters[i] : "0"}</span>`;
-                } else {
-                    codeSequence.children[i].innerText = typeof(characters[i]) != "undefined" ? characters[i] : "0";
-                }
-            }
+          codeSequence.children[i].innerText =
+            typeof characters[i] != "undefined" ? characters[i] : "0";
         }
+      }
     }
-    else {
-        baseSequence.classList.remove("green");
-        newCode();
-    }
-    answerPhase = !answerPhase;
+  } else {
+    baseSequence.classList.remove("green");
+    newCode();
+  }
+  answerPhase = !answerPhase;
 }
 
 currN = "";
 // generates a new binary code
 function newCode() {
-    characters = "";
-    currN = Math.floor(Math.random() * 256);
-    // let currCode = n.toString(2).padEnd(8, "0");
-    baseSequence.innerText = currN;
-    displayBinary();
+  characters = "";
+  currN = Math.floor(Math.random() * 256);
+  // let currCode = n.toString(2).padEnd(8, "0");
+  baseSequence.innerText = currN;
+  displayBinary();
 }
 
 // displays the binary code
 function displayBinary() {
-    for (let i = 0; i <= 7; i++) {
-        if (typeof(characters[i]) != "undefined") {
-            codeSequence.children[i].innerText = characters[i];
-        } else {
-            codeSequence.children[i].innerHTML = '<span class="grey">0</span>';
-        }
+  for (let i = 0; i <= 7; i++) {
+    if (typeof characters[i] != "undefined") {
+      codeSequence.children[i].innerText = characters[i];
+    } else {
+      codeSequence.children[i].innerHTML = '<span class="grey">0</span>';
     }
+  }
 }
 
 // start the site
 newCode();
+
--- code-translator/scripts/morse.js
+++ code-translator/scripts/morse.js
@@ -1,107 +1,109 @@
-// TODO: its weird you have to confirm a word
-// esp because you can still type while its going on
-// maybe change the button icons so you can see it better
 const code = {
-    "a": ".-",
-    "b": "-...",
-    "c": "-.-.",
-    "d": "-..",
-    "e": ".",
-    "f": "..-.",
-    "g": "--.",
-    "h": "....",
-    "i": "..",
-    "j": ".---",
-    "k": "-.-",
-    "l": ".-..",
-    "m": "--",
-    "n": "-.",
-    "o": "---",
-    "p": ".--.",
-    "q": "--.-",
-    "r": ".-.",
-    "s": "...",
-    "t": "-",
-    "u": "..-",
-    "v": "...-",
-    "w": ".--",
-    "x": "-..-",
-    "y": "-.--",
-    "z": "--..",
-}
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+};
 
 // get wordlist, and after that, start the site
 let wordlist;
-getWords().then(words => {
+getWords()
+  .then((words) => {
     wordlist = words.split("\n");
     currword = getWord();
     displayMorse();
     displayWord();
-}).catch(() => alert("We've encountered an error. See console for details."));
+  })
+  .catch(() => alert("We've encountered an error. See console for details."));
 
 // handling fetching the wordlist
 function getWords() {
-    return new Promise((resolve, reject) => {
-        // this wordlist is from https://www.ef.com/wwen/english-resources/english-vocabulary/top-3000-words/
-        fetch('../words.txt')
-            .then(response => response.text())
-            .then(data => {
-                resolve(data);
-            })
-            .catch(err => {
-                console.log(err);
-                reject();
-            });
-    });
-};
+  return new Promise((resolve, reject) => {
+    // this wordlist is from https://www.ef.com/wwen/english-resources/english-vocabulary/top-3000-words/
+    fetch("../words.txt")
+      .then((response) => response.text())
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject();
+      });
+  });
+}
 
 let currword = "";
 // getting a word from the wordlist
 function getWord() {
-    let randomInt = Math.floor(Math.random() * wordlist.length);
-    return wordlist[randomInt].toLowerCase();
+  let randomInt = Math.floor(Math.random() * wordlist.length);
+  return wordlist[randomInt].toLowerCase();
 }
 
 // keybord input
-document.addEventListener("keydown", e => {
-    switch (e.key) {
-        case " ":
-            confirmChar();
-            break;
-        case ".":
-            addChar(".");
-            break;
-        case "-":
-            addChar("-");
-            break;
-        case "Backspace":
-            delChar();
-            break;
-    }
+document.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case " ":
+      confirmChar();
+      break;
+    case ".":
+      addChar(".");
+      break;
+    case "-":
+      addChar("-");
+      break;
+    case "Backspace":
+      delChar();
+      break;
+  }
 });
 
 // adding morse code . or -
 let characters = "";
 function addChar(char) {
-    if (currword.length <= userword.length) { return; }
-    if (characters.length != 5) characters = `${characters}${char}`;
+  if (currword.length <= userword.length) {
+    return;
+  }
+  if (characters.length != 5) characters = `${characters}${char}`;
 
-    displayMorse();
+  displayMorse();
 }
 
 // deleting character from morse code
 function delChar() {
-    characters = characters.slice(0, -1);
+  characters = characters.slice(0, -1);
 
-    displayMorse();
+  displayMorse();
 }
 
 // sending morse code feedback
 function displayMorse() {
-    for (let i = 0; i < 5; i++) {
-        codeSequence.children[i].innerText = typeof (characters[i]) != "undefined" ? characters[i] : "_";
-        if (characters[i] == ".") codeSequence.children[i].innerText = "·";
-    }
+  for (let i = 0; i < 5; i++) {
+    codeSequence.children[i].innerText =
+      typeof characters[i] != "undefined" ? characters[i] : "_";
+    if (characters[i] == ".") codeSequence.children[i].innerText = "·";
+  }
 }
 
 let userword = "";
@@ -109,42 +111,52 @@ let endOfWord = false;
 // confirming morse code to a character
 // also is the function that handles next word
 function confirmChar() {
-    if (userword.length == currword.length) endOfWord = true;
-    if (!endOfWord) {
-        let char = "";
-        Object.keys(code).forEach(key => {
-            if (code[key] == characters) {
-                char = key;
-            }
-        });
-        characters = "";
-        userword = `${userword}${char}`;
-    } else {
-        endOfWord = false;
-        currword = getWord();
-        userword = "";
-    }
+  if (endOfWord) {
+    endOfWord = false;
+    currword = getWord();
+    userword = "";
     displayMorse();
     displayWord();
+    input.children[0].style.visibility = "visible";
+    return;
+  }
+  let char = "";
+  Object.keys(code).forEach((key) => {
+    if (code[key] == characters) {
+      char = key;
+    }
+  });
+  characters = "";
+  userword = `${userword}${char}`;
+  displayMorse();
+  displayWord();
+  if (userword.length == currword.length) {
+    endOfWord = true;
+    input.children[0].style.visibility = "hidden";
+  }
 }
 
 // sending word feedback
 function displayWord() {
-    baseSequence.innerHTML = "";
-    for (let i = 0; i < userword.length; i++) {
-        let node = document.createElement("span");
-        node.innerText = userword[i];
-        node.classList.add(userword[i] != currword[i] ? "red" : "green");
-        // kind of from this answer https://stackoverflow.com/a/42528274/12706133
-        baseSequence.appendChild(node);
-    }
-
+  baseSequence.innerHTML = "";
+  for (let i = 0; i < userword.length; i++) {
     let node = document.createElement("span");
-    node.innerText = userword.length != currword.length ? currword[userword.length] : "";
-    node.classList.add("highlight");
+    node.innerText = userword[i];
+    node.classList.add(userword[i] != currword[i] ? "red" : "green");
+    // kind of from this answer https://stackoverflow.com/a/42528274/12706133
     baseSequence.appendChild(node);
+  }
 
-    let node2 = document.createElement("span");
-    node2.innerText = currword.slice(userword.length + 1);
-    baseSequence.appendChild(node2);
+  let node = document.createElement("span");
+  node.innerText =
+    userword.length != currword.length ? currword[userword.length] : "";
+  node.classList.add("highlight");
+  baseSequence.appendChild(node);
+
+  let node2 = document.createElement("span");
+  node2.innerText = currword.slice(userword.length + 1);
+  baseSequence.appendChild(node2);
+  size = currword.length / 5;
+  if (currword.length < 5) size = 0;
+  baseSequence.style.fontSize = `${5 - size}em`;
 }
