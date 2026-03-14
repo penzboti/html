bc58e31aea27454d2356572f39fdf7ddb977685d
penzboti <penzboti@gmail.com>
2026-03-14 22:57:47 +0100

    STUDYTEXT: headings, links images, amount saving

--- studytext/scripts.js
+++ studytext/scripts.js
@@ -1,10 +1,10 @@
 // #### config
 const replaceText = "[replace this]" // note that '<' and '>' produce different html
-const replaceAmount = 10;
 
 // #### globals
 navbar.style.visibility = "hidden";
 
+let replaceAmount = 10;
 let globaltxt = "";
 let globallist = [];
 let globaltype;
@@ -16,6 +16,53 @@ function start() {
     return;
   }
   hide()
+
+  globaltxt = globaltxt.split('').filter(c => c !== "\r").join("");
+  // parse [[links]] and ![[images]]
+  globaltxt = globaltxt.split('\n').map(l => {
+    if (l.includes('[[') && l.includes(']]')) {
+      if (l.includes('png') || l.includes('jpg')) {
+        let new_l = "";
+        let inside = false;
+        for (let i = 1; i < l.length; i++) {
+          let prev = l[i-1];
+          let cur = l[i]
+          if (prev == cur && cur == "[") inside = true
+          if (!inside) new_l += cur
+          if (prev == cur && cur == "]") {inside = false; new_l = new_l.substring(0,new_l.length-1) + "[image]" }
+        }
+        l = new_l
+      }
+      else if (l.includes('|')){
+        let new_l = "";
+        let inside = false;
+        let after = false;
+        for (let i = 1; i < l.length; i++) {
+          let prev = l[i-1];
+          let cur = l[i]
+          if (after) new_l += cur
+          if (prev == cur && cur == "[") { inside = true; new_l = new_l.substring(0,new_l.length-2) }
+          if (inside && cur == "|") { after = true; new_l = new_l.substring(0,new_l.length-2) }
+          if (prev == cur && cur == "]") { inside = false; after = false; new_l = new_l.substring(0,new_l.length-2) }
+        }
+        l = new_l
+      }
+      else {
+        let new_l = "";
+        let inside = false;
+        for (let i = 1; i < l.length; i++) {
+          let prev = l[i-1];
+          let cur = l[i]
+          if (inside) new_l += cur
+          if (prev == cur && cur == "[") { inside = true; new_l = new_l.substring(0,new_l.length-2) }
+          if (prev == cur && cur == "]") { inside = false; new_l = new_l.substring(0,new_l.length-2) }
+        }
+        l = new_l
+      }
+    }
+    return l;
+  }).join("\n")
+
   globaltype = type.value;
 
   if (globaltype == "normal") {
@@ -24,16 +71,27 @@ function start() {
     render(text);
     replaceReplacements(list);
   }
-  else if (globaltype == "bold") {
+  else if (globaltype == "bold" || globaltype == "heading" || globaltype == "bold+heading") {
     render(globaltxt);
-    let strongs = main?.getElementsByTagName("strong"); 
+    let strongs = [];
+    if (globaltype == "bold" || globaltype == "bold+heading") strongs = [...strongs, ...main?.getElementsByTagName("strong")]; 
+    if (globaltype == "heading" || globaltype == "bold+heading") strongs = [
+      ...strongs,
+      ...main?.getElementsByTagName("h1"),
+      ...main?.getElementsByTagName("h2"),
+      ...main?.getElementsByTagName("h3"),
+      ...main?.getElementsByTagName("h4"),
+      ...main?.getElementsByTagName("h5"),
+      ...main?.getElementsByTagName("h6"),
+    ]; 
+    // parse
     let all_list = [];
     for (let i = 0; i < strongs.length; i++) {
       let e = strongs[i];
       e.id = i;
       all_list.push({id: i, word: e.innerText})
     }
-    console.log(all_list)
+    // render
     let list = randomRemove(all_list)
     for (let item of list) {
       let e = document.getElementById(item.id);
@@ -111,6 +169,11 @@ fileinput?.addEventListener('change', () => {
 textinput?.addEventListener('change', () => {
   globaltxt = textinput.value;
   render(globaltxt)
+})
+
+// amount
+amount?.addEventListener('change', () => {
+  replaceAmount = amount.value;
 })
 
 // #### handling 'quiz'
@@ -200,6 +263,10 @@ setTimeout(() => {
       render(globaltxt);
     }
   }
+
+  if (amount.value) {
+    replaceAmount = amount.value
+  }
 }, 50) // because why not pageshow doesnt work like that
 });
 
@@ -242,7 +309,7 @@ function selectDates() {
         date_potential = true
       }
     }
-    if (!date_potential && word.split('').every(c => "0123456789.-,:;".includes(c))) {
+    if (!date_potential && word.split('').every(c => "0123456789.-,:;/".includes(c))) {
       if (word.split('').some(c => "0123456789".includes(c))) {
         curr_list.push({i,word,type: "number"})
         date_potential = true
@@ -253,16 +320,17 @@ function selectDates() {
       if (split[0].split('').some(c => "0123456789".includes(c))) {
         curr_list.push({i,word: split[0],type: "number",additions: `-${split[1]}`})
       }
+      date_potential = true
     }
 
-    if (!date_potential && curr_list.length !== 0) {
+    if ((!date_potential && curr_list.length !== 0) || (date_potential && i == bank.length-1)) {
       if (!curr_list.map(e => e.type).some(t => t === "number")) {
         curr_list = []
         continue
       }
       if (curr_list.length === 1) {
         let d = curr_list[0].word
-        if (d.length < 4 || (d.length > 4 && d.substring(0,4).split('').some(c => !"0123456789".includes(c)) && d[4] !== "-" )) {
+        if (d.length < 4 || (d.length > 4 && d.substring(0,4).split('').some(c => !"0123456789".includes(c)) && d[4] !== "-" && d[4] !== "/" )) {
           curr_list = []
           continue
         }
@@ -284,6 +352,7 @@ function selectDates() {
       let last = curr_list[curr_list.length-1];
       let trailing_char = last.word.split('').pop()
       if ("0123456789.".includes(trailing_char)) trailing_char = null;
+      if (last.type == "month") trailing_char = null;
       if (trailing_char) date = date.substring(0,date.length-1)
       if (last.additions) trailing_char = last.additions
       list.push({date,indexes,includes_year: curr_list[0].type === "number",trailing_char})
